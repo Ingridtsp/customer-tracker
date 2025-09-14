e15c47fbccb4900176e7a9fd33b63d85fabb5996
Ingridtsp <ingridnicole@usf.edu>
2025-09-13 22:07:39 -0400

    completed step 2 customer records.

--- cc_3.js
+++ cc_3.js
@@ -1,2 +1,14 @@
 //coding challenge 3a
 
+// Step 2: create customer records 
+let customer= [
+    {name:"Ingrid Gonzalez",
+        email:"ingrid.gonzalez@email.com",
+        purchases: ["Ipad", "Ipad case"]},
+        {name:"Jade Hidalgo",
+            email:"jade.hidalgo@email.com",
+            purchases: ["coffee maker", "coffee filters"]},
+            {name:"Jonathan Hidalgo",
+                email:"jonathan.hidalgo@email.com",
+                purchases:["car oil", "spark plugs", "jumping cables"]}
+]
